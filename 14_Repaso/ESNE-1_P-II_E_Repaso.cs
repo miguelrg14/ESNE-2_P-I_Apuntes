5e4e6f327de24a7d9d9b7e3f70ac38f6a9c510d2
Miguel Rodríguez Gallego <miguelrodriguezgallego1@gmail.com>
2025-08-19 17:23:55 +0200

    Proyectos

---
+++ 14_Repaso/ESNE-1_P-II_E_Repaso.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+namespace Prog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaramos e inicializamos el fichero de lectura
+            StreamReader ficheroDeLectura;
+            ficheroDeLectura = File.OpenText("text.txt");
+
+            // Declaramos e inicializamos nuestra estructura de datos estática
+            int[] nums = new int[5];
+
+            // Leemos los valores del fichero
+            for (int aux = 0; aux < nums.Length; aux++)
+            {
+                nums[aux] = Convert.ToInt32(ficheroDeLectura.ReadLine());
+            }
+            ficheroDeLectura.Close();
+
+            // Ordenamos el array
+            BubbleSort(ref nums);
+
+            // Declaramos e inicializamos el fichero de escritura
+            StreamWriter ficheroDeEscritura;
+            ficheroDeEscritura = File.AppendText("text.txt");
+
+            // Escribimos en el fichero de salida
+            ficheroDeEscritura.WriteLine();
+            for (int aux = 0; aux < nums.Length; aux++)
+            {
+                ficheroDeEscritura.Write(nums[aux]);
+                if(aux < nums.Length - 1)
+                {
+                    ficheroDeEscritura.Write(" / ");
+                }
+            }
+            ficheroDeEscritura.Close();
+        }
+
+        /// <summary>
+        /// Método que ordena los valores de un array de enteros de menor a mayor
+        /// </summary>
+        /// <param name="arrayToOrder"> Array para ser ordenado </param>
+        static void BubbleSort(ref int[] arrayToOrder)
+        {
+            bool somethingChanged = true; // Variable de control del algoritmo de ordenación
+
+            // Bucle principal, termina cuando todo esta ordenado
+            while (somethingChanged)
+            {
+                somethingChanged = false;
+
+                // Bucle de comparaciones (el -1 es para no salirnos del array)
+                for (int aux = 0; aux < arrayToOrder.Length - 1; aux++)
+                {
+                    // Se cambian los valores de posición
+                    if (arrayToOrder[aux] > arrayToOrder[aux + 1])
+                    {
+                        int first = arrayToOrder[aux];
+                        arrayToOrder[aux] = arrayToOrder[aux + 1];
+                        arrayToOrder[aux + 1] = first;
+
+                        somethingChanged = true; // Se sigue ejecutando el algortimo
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 7_Métodos/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _7._Ejercicios_métodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ejercicios métodos
+            int inputNum;
+
+            /// 1 ; Cuadrado
+            int resultado;
+
+            Console.WriteLine("Dame un número");
+            inputNum = Convert.ToInt32(Console.ReadLine());
+
+            resultado = Cuadrado(inputNum);
+            // Los parentesis definen las variables
+
+            Console.WriteLine(resultado);
+
+            /// 2; Potencias
+            int potencia;
+
+            Console.WriteLine("Dame un número x");
+            inputNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dame un número y");
+            potencia = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(inputNum + " elevado a " + potencia + " = " + elevado(inputNum, potencia));
+        }
+
+        // Método que calcula el cuadrado de un número "num"
+        static int Cuadrado(int num)
+        {
+            return num * num;
+        }
+
+        static int elevado(int x, int y)
+        {
+            int res = 0;
+
+            for (int aux = 0; aux < y; aux++)   // 0, 1 y 2
+            {
+                res *= x; // res = res * x;
+            }
+
+            return res;
+        }
+    }
+}
